bbc510aa7b8fe37da24cc49a564449d1fce80836
Likitha Khatri <likithakhatri5774@gmail.com>
2023-09-04 20:38:12 +0530

    Check if a number is Prime or not

---
+++ Functions/primeORnot.java
@@ -0,0 +1,20 @@
+// Check if a number is Prime or not 
+
+public class primeORnot{
+    public static boolean isPrime(int n){
+
+        if(n == 2){
+            return true;
+        }
+
+        for(int i=2; i<=n-1; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main (String args[]){
+        System.out.println(isPrime(2));
+    }
+}
