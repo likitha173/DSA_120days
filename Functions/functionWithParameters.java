2025fdfa14e234936c6edf1638cfa9b4ae9cb77f
Likitha Khatri <likithakhatri5774@gmail.com>
2023-09-04 18:26:14 +0530

    Call by Value in Java

--- Functions/functionWithParameters.java
+++ Functions/functionWithParameters.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class functionWithParameters{
-    public static int sumOf2Nums(int num1, int num2){
+    public static int sumOf2Nums(int num1, int num2){  // Parameters or formal Parameters
         int sum = num1 + num2;
         return sum;
     }
@@ -10,7 +10,7 @@ public class functionWithParameters{
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = sumOf2Nums(a, b);
+        int sum = sumOf2Nums(a, b);  // Arguments or Actual Parameters
         System.out.println("Sum is : " + sum);
     }
 }
