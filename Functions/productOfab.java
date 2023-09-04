1f4a6f768e501d684bed5c3d33857931934c92d2
Likitha Khatri <likithakhatri5774@gmail.com>
2023-09-04 18:35:13 +0530

    Product of a & b using functions

---
+++ Functions/productOfab.java
@@ -0,0 +1,13 @@
+public class productOfab{
+    public static int multiply(int a, int b){
+        int product = a * b;
+        return product;
+    }
+
+    public static void main(String args[]){
+        int a = 3;
+        int b = 5;
+        int prod = multiply(a, b);
+        System.out.println("Product of a * b = " + prod);
+    }
+}
