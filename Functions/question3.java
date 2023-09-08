491af7df2bde0a692cb36af2c0b2ffd63b883d39
Likitha Khatri <likithakhatri5774@gmail.com>
2023-09-08 05:57:49 +0530

    is given number is palindrome

--- Functions/question3.java
+++ Functions/question3.java
@@ -1,3 +1,11 @@
+/*\
+Question 3 : Write a Java program to check if a number is a palindrome in Java? ( 121 is a
+palindrome, 321 is not)
+A number is called a palindrome if the number is equal to the reverse of a number e.g., 121 is a
+palindrome because the reverse of 121 is 121 itself. On the other hand, 321 is not a
+palindrome because the reverse of 321 is 123, which is not equal to 321.
+
+ */
 
 import java.util.Scanner;
 
