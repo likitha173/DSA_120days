7a2350d4377c17aa28e7290293a19ec4ed2ad612
Likitha Khatri <likithakhatri5774@gmail.com>
2023-09-26 05:14:38 +0530

    Max subArray Sum - Kadanes Algo

---
+++ Arrays/kadanesMaxSubArrSum.java
@@ -0,0 +1,20 @@
+public class kadanesMaxSubArrSum {
+    public static void kadanes(int numbers[]){
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for(int i=0; i<numbers.length; i++){
+            currSum = currSum + numbers[i];
+            if(currSum < 0){
+                currSum = 0;
+            }
+            maxSum = Math.max(currSum, maxSum);
+        }
+
+        System.out.println("Max subArray Sum: " + maxSum);
+    }
+
+    public static void main(String[] args){
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        kadanes(numbers);
+    }
+}
