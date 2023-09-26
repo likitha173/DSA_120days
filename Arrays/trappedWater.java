84cf741a57df147b2f24c19b41589346d2e5702f
Likitha Khatri <likithakhatri5774@gmail.com>
2023-09-26 05:56:37 +0530

    to be cont..

---
+++ Arrays/trappedWater.java
@@ -0,0 +1,12 @@
+public class trappedWater {
+    public static int totalTrappedWater(int height[]){
+        // calculate left max boundary
+        // calculate right max boundary - arraysCC
+        // loop
+        // waterLevel = min(leftMaxBound, rightMaxBound)
+        // trapped water = waterLevel - height[i];
+    }
+    public static void main(String args[]){
+        int height[] = {4, 2, 0, 6, 3, 2, 5};
+    }
+}
