7375d3b2c138826aa4d4d928de00fbf2a512be64
Likitha Khatri <likithakhatri5774@gmail.com>
2023-10-01 21:57:52 +0530

    Buy and Sell

---
+++ Arrays/BuyAndSellStocks.java
@@ -0,0 +1,8 @@
+public class BuyAndSellStocks {
+    public static int Stocks(int prices[]){
+        int buy
+    }
+    public static void main(String args[]){
+        int prices[] = {7, 1, 5, 3, 6, 4};
+    }
+}
