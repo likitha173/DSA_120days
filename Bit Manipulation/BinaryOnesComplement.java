e6a19d6533264a3502e25abd25bee96111ebb4e7
Likitha Khatri <likithakhatri5774@gmail.com>
2023-10-10 18:18:41 +0530

    Binary Ones Complement

---
+++ Bit Manipulation/BinaryOnesComplement.java
@@ -0,0 +1,7 @@
+public class BinaryOnesComplement {
+    public static void main(String[] args){
+        System.out.println(~5);
+        System.out.println(~0);
+        System.out.println(~1);
+    }       
+}
