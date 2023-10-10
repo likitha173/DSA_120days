8c99c8def670e6849b6c8df5cd79459190913c4e
Likitha Khatri <likithakhatri5774@gmail.com>
2023-10-10 19:07:28 +0530

    Clear ith Bit

---
+++ Bit Manipulation/ClearithBit.java
@@ -0,0 +1,9 @@
+public class ClearithBit {
+    public static void main(String[] args){
+        int n = 10;
+        int i = 1;
+        int bitMask = ~(1<<i);
+
+        System.out.println(n & bitMask);
+    }    
+}
