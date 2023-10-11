fb75d944f38cdb40363a99bab4c1cfb38fd4c3b5
Likitha Khatri <likithakhatri5774@gmail.com>
2023-10-11 15:58:52 +0530

    Clear Last ith Bit

---
+++ Bit Manipulation/ClearLastithBit.java
@@ -0,0 +1,9 @@
+public class ClearLastithBit {
+    public static void main(String args[]){
+        int n = 15;
+        int i = 2;
+        int bitMask = (~0)<<i;
+        
+        System.out.println(n & bitMask);
+    }    
+}
