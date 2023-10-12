4a96d6ca15c4c5e144fd24f201b351531ac8cc12
Likitha Khatri <likithakhatri5774@gmail.com>
2023-10-12 15:40:58 +0530

    OOPs 12/10/23

---
+++ OOPS Java/OOPS.java
@@ -0,0 +1,20 @@
+public class OOPS {
+    public static void main(String ages[]){
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+    }    
+}
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
