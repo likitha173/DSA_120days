5b132af12c922e715521bceecf0194f327737587
Likitha Khatri <likithakhatri5774@gmail.com>
2023-10-26 00:54:28 +0530

    n Queens

--- Backtracking Java/nQueens.java
+++ Backtracking Java/nQueens.java
@@ -1,13 +1,33 @@
 public class nQueens {
+    public static boolean isSafe(char board[][], int row, int col){
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void nQueen(char board[][], int row){
         if(row == board.length){
             printBoard(board);
             return;
         }
         for(int j=0; j<board.length; j++){
+            if(isSafe(board, row, j)){
             board[row][j] = 'Q';
             nQueen(board, row+1);
             board[row][j] = 'x';
+            }
         }
     }
     public static void printBoard(char board[][]){
@@ -20,7 +40,7 @@ public class nQueens {
         }
     }
     public static void main(String args[]){
-        int n = 2;
+        int n = 4;
         char board[][] = new char[n][n];
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
