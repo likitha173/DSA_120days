424798b3347bd18c104f5dd205b24a768b014068
Likitha Khatri <likithakhatri5774@gmail.com>
2023-10-29 23:37:05 +0530

    LinkedList

--- Linked List/LinkedList.java
+++ Linked List/LinkedList.java
@@ -12,7 +12,51 @@ public class LinkedList {
     public static Node tail;  
 
     //Methods
+
+    public void addFirst(int data){  //O(1)
+        //  step1 = create new node
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        // step2 = newNode next = head
+        newNode.next = head;
+
+        // step3 - head = newNode
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
     }
 }
