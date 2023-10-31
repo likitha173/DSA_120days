6f0be802814efc4684857b78f7bda0e38114f8f0
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-01 01:22:18 +0530

    Detect Cycle in Linked List

--- Linked List - 2/DetectCycle.java
+++ Linked List - 2/DetectCycle.java
@@ -1,20 +1,20 @@
 public class DetectCycle {
-    public static class  Node {
+    public static class Node {
         int data;
         Node next;
-        
-        public Node(int data){
+
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
-    }  
+    }
 
-    public static Node head;  
-    public static Node tail; 
-    
-    public void addLast(int data){
+    public static Node head;
+    public static Node tail;
+
+    public void addLast(int data) {
         Node newNode = new Node(data);
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -22,29 +22,61 @@ public class DetectCycle {
         tail = newNode;
     }
 
-
-    public static boolean isCycle(){
+    public static boolean isCycle() {
         Node slow = head;
         Node fast = head;
 
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast){
+            if (slow == fast) {
                 return true;
             }
         }
         return false;
     }
 
-    public static void main(String args[]){
-        head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = head;
+    public static void removeCycle() {
+        // detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
 
-        // 1->2->3->1
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                cycle = true;
+                break;
+            }
+        }
+
+        if (cycle == false) {
+            return;
+        }
+
+        slow = head;
+        Node prev = null;
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        prev.next = null;
+    }
+
+    public static void main(String args[]) {
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+
+        // 1->2->3->2
         System.out.println(isCycle());
-     
+        removeCycle();
+        System.out.println(isCycle());
+
     }
 }
