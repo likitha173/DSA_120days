f16b6e1520bf544982625bab3b2d4a40a72e82f9
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-11 15:07:18 +0530

    Print Root to Leaf

---
+++ Binary Search Tree/PrintRoot2Leaf.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class PrintRoot2Leaf {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static Node insert(Node root, int val) {
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void printPath(ArrayList<Integer> path){
+        for(int i=0; i<path.size(); i++){
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("null");
+    }
+    public static void root2leaf(Node root, ArrayList<Integer> path){
+        if(root == null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+        root2leaf(root.left, path);
+        root2leaf(root.right, path);
+        path.remove(path.size() - 1);
+    }
+    public static void main(String[] args) {
+        int values[] = {8, 5, 3, 6, 10, 11, 14};
+        Node root = null;
+
+        for(int i=0; i<values.length; i++){
+            root = insert(root, values[i]);
+        }
+
+        root2leaf(root, new ArrayList<>());
+    }    
+}
