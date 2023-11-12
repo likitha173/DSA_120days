0484e7ce4e01107c4024b5d020c621d44fbec57e
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-12 15:20:43 +0530

    Priority Queues Integer

---
+++ Heaps/PriorityQueuesInt.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class PriorityQueuesInt {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek());
+            pq.remove();
+        }  
+    }
+}
