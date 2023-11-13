911445f470d3faf002b027e005eaccc4386a3ccd
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-14 00:18:50 +0530

    Majority Element

---
+++ Hashing/MajorityElement.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MajorityElement {  // O(n)
+    public static void main(String[] args) {
+        int nums[] = {1, 3, 2, 5, 1, 3, 1, 5, 1};
+        // int nums[] = {1, 2};
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for(Integer key : map.keySet()){
+            if(map.get(key) > nums.length/3){
+                System.out.println(key);
+            }
+        }
+    }
+}
