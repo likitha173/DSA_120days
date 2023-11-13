cb0b1b3275c8b06dc429d7f0e19c268dee39d156
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-13 23:47:57 +0530

    TreeMaps

---
+++ Hashing/TreeMaps.java
@@ -0,0 +1,12 @@
+import java.util.TreeMap;
+
+public class TreeMaps {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> hm = new TreeMap<>();
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("US", 50);
+
+        System.out.println(hm);
+    }
+}
