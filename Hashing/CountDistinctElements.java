9875d88567dea322ff3b453c91b4e4bb3e74ce5b
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-14 18:38:43 +0530

    Count Distinct Elements

---
+++ Hashing/CountDistinctElements.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class CountDistinctElements {
+    public static void main(String[] args) {
+        int nums[] = {4, 3, 2, 5, 6, 7, 3, 4, 2, 1};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
+        }
+
+        System.out.println("Ans: " + set.size());
+    }
+}
