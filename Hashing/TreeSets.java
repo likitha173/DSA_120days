866a159cc0514cb4ff6e33e367782128596bdf47
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-14 17:11:20 +0530

    TreeSet

---
+++ Hashing/TreeSets.java
@@ -0,0 +1,14 @@
+import java.util.TreeSet;
+
+public class TreeSets {
+    public static void main(String[] args) {
+        TreeSet<String> cities = new TreeSet<>();
+
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Noida");
+        cities.add("Bengaluru");
+
+        System.out.println(cities);
+    }
+}
