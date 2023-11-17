8fdbced24f9a19dfb5dfdf98413adcb64f7d43f4
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-18 00:03:05 +0530

    DFS Connect Components

---
+++ Graphs 1/DFSConnectComponents.java
@@ -0,0 +1,73 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class DFSConnectComponents {
+    static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d, int w){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]){
+            for(int i=0; i<graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+
+        graph[1].add(new Edge(1, 0, 1));
+        graph[1].add(new Edge(1, 3, 1));
+
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
+        
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
+        
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
+        
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[6].add(new Edge(6, 5, 1));
+    }
+
+    public static void dfs(ArrayList<Edge>[] graph){
+        boolean visited[] = new boolean[graph.length];
+        for(int i=0; i<graph.length; i++){
+            dfsUtil(graph, i, visited);
+        }
+    }
+    //O(V+E)
+    public static void dfsUtil(ArrayList<Edge> graph[], int curr, boolean visited[]){
+        System.out.print(curr + " ");
+        visited[curr] = true;
+        
+        for(int i=0; i<graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(!visited[e.dest]){
+                dfsUtil(graph, e.dest, visited);
+            }
+        }
+      
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        dfsUtil(graph, 0, new boolean[V]);
+    }
+}
