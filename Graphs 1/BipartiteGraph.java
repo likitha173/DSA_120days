eaa479b266ee84da62ce2980b4fbe6679cba7b9d
Likitha Khatri <likithakhatri5774@gmail.com>
2023-11-18 01:02:31 +0530

    Bipartite Graph

---
+++ Graphs 1/BipartiteGraph.java
@@ -0,0 +1,96 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class BipartiteGraph {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]){
+            for(int i=0; i<graph.length; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+        
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+        
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+       
+    }
+
+    public static boolean isBipartite(ArrayList<Edge>[] graph){
+        int color[] = new int[graph.length];
+        for(int i=0; i<color.length; i++){
+            color[i] = -1;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0; i<graph.length; i++){
+            if(color[i] == -1){
+                q.add(i);
+                color[i] = 0;
+                while (!q.isEmpty()) {
+                    int curr = q.remove();
+                    for(int j=0; j<graph[curr].size(); j++){
+                        Edge e = graph[curr].get(j);
+                        if(color[e.dest] == -1){
+                            int nextColor = color[curr] == 0 ? 1 : 0;
+                            color[e.dest] = nextColor;
+                            q.add(e.dest);
+                        } else if(color[e.dest] == color[curr]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+
+    //O(V+E)
+    public static boolean detectCycleUtil(ArrayList<Edge> graph[], boolean visited[], int curr, int prerent){
+        visited[curr] = true;
+        
+        for(int i=0; i<graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(!visited[e.dest]){
+                if(detectCycleUtil(graph, visited, e.dest, curr)){
+                    return true;
+                }
+            }
+
+            else if(visited[e.dest] && e.dest != prerent){
+                return true;
+            }
+        }
+        return false;
+      
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        System.out.println(isBipartite (graph));
+    }
+}
+
